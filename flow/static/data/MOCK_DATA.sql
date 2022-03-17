36da9402f10dec5889d445ddbadeea778f005950
muturi25 <petermuturi74@gmail.com>
2022-03-17 13:34:57 +0300

    update gitignore with medai directory

---
+++ flow/static/data/MOCK_DATA.sql
@@ -0,0 +1,10 @@
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (1, 'Bendite', 'Gilbanks', 'bgilbanks0@cnn.com', 'Male', '90.96.93.6');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (2, 'Ninon', 'Round', 'nround1@free.fr', 'Male', '80.93.38.179');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (3, 'Ashlee', 'Heliot', 'aheliot2@lulu.com', 'Polygender', '103.18.243.158');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (4, 'Erroll', 'Pareman', 'epareman3@deviantart.com', 'Male', '128.14.225.203');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (5, 'Emelen', 'Weich', 'eweich4@telegraph.co.uk', 'Male', '98.34.196.203');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (6, 'Hortensia', 'Mayoh', 'hmayoh5@uol.com.br', 'Male', '170.65.215.29');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (7, 'Yanaton', 'Cotton', 'ycotton6@youtube.com', 'Male', '229.159.159.254');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (8, 'Ernst', 'Hollebon', 'ehollebon7@usda.gov', 'Male', '161.209.32.235');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (9, 'Prue', 'Simons', 'psimons8@mayoclinic.com', 'Male', '86.120.52.22');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (10, 'Harald', 'Franz', 'hfranz9@vistaprint.com', 'Female', '237.201.246.234');
--- flow/views.py
+++ flow/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 from flow.forms import DocumentForm
 
 # Create your views here.
@@ -10,7 +13,9 @@ def file_upload(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
+            print(form.files.values)
             form.save()
+            form = DocumentForm()
             return redirect('index')
     else:
         form = DocumentForm()
