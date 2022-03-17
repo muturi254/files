c2b382ac54f786afaf483411de15f86c908be4c2
muturi25 <petermuturi74@gmail.com>
2022-03-17 12:20:21 +0300

    set up media Url

--- assesment/urls.py
+++ assesment/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from ctypes.wintypes import HHOOK
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,3 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('flows/', include('flow.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
